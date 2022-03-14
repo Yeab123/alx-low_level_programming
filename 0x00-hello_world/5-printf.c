e1b505ca8167a262e215e9e3d7570d799ce6e8d1
Yeabsira Nigussie <serayeab201@gmail.com>
2022-03-14 14:12:12 -0700

    my first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#includ <stdio.h>
+/**
+ * main - entry ponit
+ * My hello, printf task
+ * Return: return 1 after program execution
+ */
+int main(void)
+{
+printf("with proper grammar . but the outcome is a place of art.\n"):
+return (0):
+}
