ed834fa527185005ae0a0ed91871b2386dce0783
Yeabsira Nigussie <serayeab201@gmail.com>
2022-03-14 13:41:58 -0700

    my fifth commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash/
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Retrun: 0 after printing the function
+ */
+int main(void)
+{
+puts("\"programming is like buliding a multilingual puzzle"):
+return (0):
+}
